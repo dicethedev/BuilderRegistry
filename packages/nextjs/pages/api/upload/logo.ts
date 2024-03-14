a15187b4e7d2fc44ec20e07c217ae6855c1b15a2
Angry Coding <mailidanielifechukwu@gmail.com>
2024-03-14 03:16:29 +0100

    Woked on upload logo and  uplad documents

---
+++ packages/nextjs/pages/api/upload/logo.ts
@@ -0,0 +1,80 @@
+import formidable from "formidable";
+import { NextApiRequest, NextApiResponse, PageConfig } from "next";
+import { Writable } from "stream";
+import "~~/services/firebase";
+import { uploadFile } from "~~/services/storage";
+
+const formidableConfig = {
+  keepExtensions: true,
+  maxFileSize: 10_000_000,
+  maxFieldsSize: 10_000_000,
+  maxFields: 3,
+  allowEmptyFiles: false,
+  multiples: false,
+};
+
+function formidablePromise(
+  req: NextApiRequest,
+  opts?: Parameters<typeof formidable>[0],
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> {
+  return new Promise((accept, reject) => {
+    const form = formidable(opts);
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err);
+      }
+      return accept({ fields, files });
+    });
+  });
+}
+
+const fileConsumer = <T = unknown>(acc: T[]) => {
+  const writable = new Writable({
+    write: (chunk, _enc, next) => {
+      acc.push(chunk);
+      next();
+    },
+  });
+
+  return writable;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") return res.status(404).end();
+  console.log(" POST /api/logo");
+  try {
+    const chunks: never[] = [];
+
+    const { files } = await formidablePromise(req, {
+      ...formidableConfig,
+      fileWriteStreamHandler: () => fileConsumer(chunks),
+    });
+
+    // console.log(fields);
+    //console.log(files);
+    const { file } = files;
+    //console.warn(file);
+    if (!file || !file[0]) {
+      return res.status(400).json({ error: "No File Provided" });
+    }
+
+    if (file[0].size > 5 * 1024 * 1024) {
+      return res.status(400).json({ error: "File size exceeds the limit of 5 MB." });
+    }
+
+    console.log(chunks, uploadFile);
+    //const fileData = Buffer.concat(chunks);
+    // const filePath = await uploadFile(file[0], fileData );
+    return res.status(201);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export const config: PageConfig = {
+  api: {
+    bodyParser: false,
+  },
+};
