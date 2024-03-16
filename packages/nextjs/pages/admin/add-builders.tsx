59e71773184801e710504494fe0a6c83337074ec
Angry Coding <mailidanielifechukwu@gmail.com>
2024-03-16 14:06:15 +0100

    Added Admin Pages

---
+++ packages/nextjs/pages/admin/add-builders.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const AddBuilder: NextPage = () => {
+  return (
+    <div>
+      <MetaHeader />
+    </div>
+  );
+};
+
+export default AddBuilder;
---
+++ packages/nextjs/pages/admin/add-notification.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { MetaHeader } from "~~/components/MetaHeader";
+
+const AddNotification: NextPage = () => {
+  return (
+    <div>
+      <MetaHeader />
+    </div>
+  );
+};
+
+export default AddNotification;
