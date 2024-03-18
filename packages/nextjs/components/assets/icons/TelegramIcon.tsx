bbafb4e874d92ca3ce740905d2bf6a8c4ca93926
Angry Coding <mailidanielifechukwu@gmail.com>
2024-03-18 02:36:22 +0100

    Icons Files

---
+++ packages/nextjs/components/assets/icons/TelegramIcon.tsx
@@ -0,0 +1,19 @@
+const TelegramIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21.2391 2.625H25.0984L16.6669 12.2617L26.5859 25.375H18.8194L12.7364 17.4218L5.77615 25.375H1.91448L10.9328 15.0675L1.41748 2.625H9.38115L14.8796 9.8945L21.2391 2.625ZM19.8846 23.065H22.0231L8.21915 4.81367H5.92431L19.8846 23.065Z"
+        fill="#1D072E"
+      />
+    </svg>
+  );
+};
+
+export default TelegramIcon;
